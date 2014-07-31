ec38c232aa3816474d05cb46b1480bb607af3ef3
Guy John <guy@rumblesan.com>
2014-07-31 12:57:08 +0100

    Slightly neater way to load class

--- src/main/g8/src/main/scala/App.scala
+++ src/main/g8/src/main/scala/App.scala
@@ -6,7 +6,7 @@ object App {
 
   def main(args: Array[String]) {
     PApplet.main(
-      Array("$organization$.$name;format="lower,word"$.$name;format="Camel,word"$")
+      Array(classOf[$organization$.$name;format="lower,word"$.$name;format="Camel,word"$].getName)
     )
   }
 
